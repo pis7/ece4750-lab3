f8c0f3d3251fc7d1bcbfb5134f95a96d2029c1e8
gpm58 <gpm58@cornell.edu>
2023-11-15 04:23:13 -0500

    Make clean with da test cases :o

--- lab3_cache/obj_dir/verilated_cov.d
+++
@@ -1,9 +0,0 @@
-verilated_cov.o: \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_cov.cpp \
- /classes/ece4750/2023.07-2/share/verilator/include/verilatedos.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_cov.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_config.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_types.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_funcs.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_cov_key.h
