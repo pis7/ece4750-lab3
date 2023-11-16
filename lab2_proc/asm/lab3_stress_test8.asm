5bd6500cce4bdda1c7e7ba075ec6f4dbc4de97b2
gpm58 <gpm58@cornell.edu>
2023-11-16 07:23:01 -0500

    More tests : )

---
+++ lab2_proc/asm/lab3_stress_test8.asm
@@ -0,0 +1,10 @@
+csrr x1, mngr2proc < 0x2000
+csrr x6, mngr2proc < 0
+csrr x7, mngr2proc < 200
+
+big_loop:
+lw x7, 0(x1)
+addi x6, x6, 1
+bne x6, x7, big_loop
+
+csrw proc2mngr, x6 > 200
--- lab3_cache/obj_dir_FL/verilated_timing.d
+++
@@ -1,8 +0,0 @@
-verilated_timing.o: \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_timing.cpp \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_timing.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilatedos.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_config.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_types.h \
- /classes/ece4750/2023.07-2/share/verilator/include/verilated_funcs.h
